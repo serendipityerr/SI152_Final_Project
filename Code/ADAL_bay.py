08340fbbdc9504710855d6e60fe53e20786fd86e
gaoshh711 <gaoshh1@gmail.com>
2025-01-05 11:25:05 +0800

    upd adal_bay

--- Code/ADAL_bay.py
+++ Code/ADAL_bay.py
@@ -8,14 +8,10 @@ from scipy.optimize import minimize
 from skopt import gp_minimize
 from skopt.space import Real
 import matplotlib.pyplot as plt
-from IRWA import initialize_experiment
-
-# np.random.seed(42)
-def objective_value(g, H, x):
-    return 0.5 * np.dot(np.dot(x.T, H), x) + np.dot(g, x)
+from func import initialize_experiment, objective, experimentSetup
 
 
-def adal_solver(A1, A2, b1, b2, g, H, M1=10, M2=10):
+def adal_solver(A1, b1, A2, b2, g, H, M1=1, M2=1):
     A = np.concatenate((A1, A2))
     b = np.concatenate((b1, b2))
     mu = 1
@@ -72,67 +68,26 @@ def adal_solver(A1, A2, b1, b2, g, H, M1=10, M2=10):
     return x, p, u
 
 
-A1, b1, A2, b2, g, H = initialize_experiment("numpy")
-pass
+def adal_solver_bay(A1, b1, A2, b2, g, H):
+    experimentPara = experimentSetup(A1, b1, A2, b2, g, H)
 
-def experimentSetup():
-    # 生成实验数据
-    global A1, b1, A2, b2, g, H
+    def objective_func(params):
+        M1, M2 = params
+        A1, b1, A2, b2, g, H = experimentPara.para()
+        x, p, u = adal_solver(A1, b1, A2, b2, g, H, M1, M2)
 
-    return A1, A2, b1, b2, g, H
+        constraint_violation_1 = np.linalg.norm(A1 @ x + b1)
+        constraint_violation_2 = np.sum(np.maximum(0, A2 @ x + b2))
+        value = g.T @ x + 0.5 * x.T @ H @ x
+        return value + constraint_violation_1 * 1000 + constraint_violation_2 * 1000
+        if constraint_violation_1 > 0 or constraint_violation_2 > 0:
+            return 0
+        else:
+            return value
 
-
-maxValue = 0
-
-
-def objective(params):
-    global maxValue
-    M1, M2 = params
-    A1, A2, b1, b2, g, H = experimentSetup()
-    x, p, u = adal_solver(A1, A2, b1, b2, g, H, M1, M2)
-
-    constraint_violation_1 = np.linalg.norm(A1 @ x + b1)
-    constraint_violation_2 = np.sum(np.maximum(0, A2 @ x + b2))
-    value = g.T @ x + 0.5 * x.T @ H @ x
-    return value + constraint_violation_1 * 1000 + constraint_violation_2 * 1000
-
-
-param_space = [
-    Real(10, 100.0, name="M1"),  # M1的取值范围
-    Real(10, 100.0, name="M2"),  # M2的取值范围
-]
-# 使用贝叶斯优化方法
-result = gp_minimize(objective, param_space, n_calls=100, random_state=42)
-
-# 输出最佳的 M1 和 M2 值
-best_M1, best_M2 = result.x
-print(f"Optimal M1: {best_M1}, Optimal M2: {best_M2}")
-
-# 打印最终的目标函数值
-print(f"Best objective value: {result.fun}")
-A1, A2, b1, b2, g, H = experimentSetup()
-x, p, u = adal_solver(A1, A2, b1, b2, g, H, best_M1, best_M2)
-print(x, p, u)
-A1_matrix = matrix(A1)
-b1_matrix = matrix(-b1)
-A2_matrix = matrix(A2)
-b2_matrix = matrix(-b2)
-g_matrix = matrix(g)
-H_matrix = matrix(H)
-
-cvxopt_sol = solvers.qp(H_matrix, g_matrix, A2_matrix, b2_matrix, A1_matrix, b1_matrix)
-cvxopt_x = np.array(cvxopt_sol["x"]).flatten()
-
-print("Optimal Sol")
-print("Result: ", cvxopt_x)
-print("objective:", objective_value(g, H, cvxopt_x))
-print("ADAL Bay")
-print("Result: ", x)
-print("objective:", objective_value(g, H, x))
-# print("Norm: ", np.linalg.norm(cvxopt_x - x))
-
-print("Fixed M1, Fixed M2")
-x, p, u = adal_solver(A1, A2, b1, b2, g, H)
-print("Result: ", x)
-print("objective:", objective_value(g, H, x))
-# 画出目标函数的收敛过程
+    result = gp_minimize(
+        objective_func, experimentPara.bay_para(), n_calls=100, random_state=42
+    )
+    best_M1, best_M2 = result.x
+    x, p, u = adal_solver(A1, b1, A2, b2, g, H, best_M1, best_M2)
+    return x, p, u, best_M1, best_M2
